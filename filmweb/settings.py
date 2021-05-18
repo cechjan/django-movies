83dc739ced393caba8c211bee5b29d2bfc8f1b45
cechjan <cechjan.c@seznam.cz>
2021-05-18 10:39:41 +0200

    11 - Přihlašování, registrace

--- filmweb/settings.py
+++ filmweb/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'movies',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,3 +130,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
